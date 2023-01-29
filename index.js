30454d980c74d3bce295245a4e10f5997caaa4e5
Mayur Jain <jainmayur517@gmail.com>
2023-01-30 01:31:58 +0530

    commit first
    demoService

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const loggerDetail = require('./util/LoggerConnect');
+
+const server = http.createServer((req, res) => {
+      loggerDetail.logInfo('info', req);
+
+});
+
+// try {
+//     console.log(a==b)
+// } catch (err) {
+//     loggerDetail.logError('error',err)
+// }
+
+process.on('uncaughtException', async (err) => {
+    console.error('Uncaught Exception: ', err);
+    await loggerDetail.logError('error',err)
+    process.exit(1);
+  });
+  
+server.listen(8000);
+
+
+
+
+// const kafka = require('kafka-node');
+// const client = new kafka.KafkaClient({ zookeeper: 'localhost:2181' });
+
+// const producer = new kafka.Producer(client);
+
+// producer.on('ready', function() {
+//   console.log("Producer is ready");
+//   producer.send([{ topic: 'test', messages: 'Hello Kafka' }], function (err, data) {
+//     if(err)
+//     console.log(err)
+//     else{
+//     console.log(`hiiiiii`)
+//     console.log(data);
+//     }
+//   });
+// });
+
+// producer.on('error', function(err) {
+//   console.log("Producer is in error state");
+//   console.log(err);
+// })
+
+// console.log("trying")
+
+
+// const consumer = new kafka.Consumer(client, [{ topic: 'test', partition: 0 }], {
+//     autoCommit: true
+//   });
+//   consumer.on('message', function (message) {
+//     console.log(message);
+//   });
---
+++ util/LoggerConnect.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+require('dotenv').config()
+
+const urlLogger='http://localhost:3000/log';
+
+const serviceName=process.env.Service_Name;
+module.exports= class loggerDetail{
+
+static async logInfo(level, req){
+  const ipUser=req.socket.localAddress;
+    const host = req.headers.host;
+    const route = req.url;
+    const sender = req.headers['user-agent'];
+    const message={ipUser, host, route, sender, serviceName };
+
+  axios.post(urlLogger, {
+    level,
+    message
+  })
+  .then(response => {
+    console.log(response.data.message);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+}
+
+
+  static async logError(level, error){
+  
+   let err= error.toString();
+   const message={err,serviceName};
+
+    axios.post(urlLogger, {
+      level,
+      message
+      
+    })
+    .then(response => {
+      console.log(response.data.message);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+}
+
+}
